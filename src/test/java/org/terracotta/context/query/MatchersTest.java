3608c7a0d90f97a1e3777ced70f3e82a1ec2e07a
Chris Dennis <chris.w.dennis@gmail.com>
2012-11-19 16:23:32 +0000

    Add partial matchers coverage
    git-svn-id: https://svn.terracotta.org/repo/forge/projects/statistics/trunk@37554 591de9ec-d6d5-471c-8416-afe4a41b277d

--- src/test/java/org/terracotta/context/query/MatchersTest.java
+++ src/test/java/org/terracotta/context/query/MatchersTest.java
@@ -4,10 +4,69 @@
  */
 package org.terracotta.context.query;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  *
  * @author cdennis
  */
 public class MatchersTest {
   
+  @Test
+  public void testSubclassOfSameClass() {
+    assertTrue(Matchers.subclassOf(Integer.class).matches(Integer.class));
+  }
+
+  @Test
+  public void testSubclassOfSuperClass() {
+    assertTrue(Matchers.subclassOf(Number.class).matches(Integer.class));
+  }
+
+  @Test
+  public void testSubclassOfInterface() {
+    assertTrue(Matchers.subclassOf(Comparable.class).matches(Integer.class));
+  }
+
+  @Test
+  public void testSubclassOfUnrelatedInterface() {
+    assertFalse(Matchers.subclassOf(CharSequence.class).matches(Integer.class));
+  }
+
+  @Test
+  public void testSubclassOfUnrelatedClass() {
+    assertFalse(Matchers.subclassOf(Runtime.class).matches(Integer.class));
+  }
+  
+  @Test
+  public void testHasAttributeOnEmptyMap() {
+    assertFalse(Matchers.hasAttribute("foo", "bar").matches(Collections.emptyMap()));
+  }
+  
+  public void testHasAttributeOnSingletonMatchingMap() {
+    assertTrue(Matchers.hasAttribute("foo", "bar").matches(Collections.singletonMap("foo", "bar")));
+  }
+  
+  public void testHasAttributeOnSingletonNonMatchingMap() {
+    assertFalse(Matchers.hasAttribute("foo", "bar").matches(Collections.singletonMap("foo", "baz")));
+  }
+  
+  public void testHasAttributeOnMatchingMap() {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("foo", "bar");
+    map.put("alice", "bob");
+    assertTrue(Matchers.hasAttribute("foo", "bar").matches(map));
+  }
+
+  public void testHasAttributeOnNonMatchingMap() {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("foo", "baz");
+    map.put("alice", "bob");
+    assertTrue(Matchers.hasAttribute("foo", "bar").matches(map));
+  }
 }
