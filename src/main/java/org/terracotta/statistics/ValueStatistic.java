3b0ed3d5ccf3ed32360caa9636892c2a241877ff
Chris Dennis <chris.w.dennis@gmail.com>
2012-11-29 17:43:23 +0000

    add a copyright header to all files
    git-svn-id: https://svn.terracotta.org/repo/forge/projects/statistics/trunk@37643 591de9ec-d6d5-471c-8416-afe4a41b277d

--- src/main/java/org/terracotta/statistics/ValueStatistic.java
+++ src/main/java/org/terracotta/statistics/ValueStatistic.java
@@ -1,6 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
  */
 package org.terracotta.statistics;
 
