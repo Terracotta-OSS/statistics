1dddd141c100e4852f37022daccbd7221132a80d
Chris Dennis <chris.w.dennis@gmail.com>
2012-12-21 20:10:02 +0000

    ide screwed up a big sequence of move, delete, replace in svn - re-add this
    git-svn-id: https://svn.terracotta.org/repo/forge/projects/statistics/trunk@37895 591de9ec-d6d5-471c-8416-afe4a41b277d

---
+++ src/main/java/org/terracotta/statistics/OperationStatistic.java
@@ -0,0 +1,51 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terracotta.statistics;
+
+import java.util.Set;
+
+import org.terracotta.statistics.observer.OperationObserver;
+
+/**
+ *
+ * @author cdennis
+ */
+public interface OperationStatistic<T extends Enum<T>> extends OperationObserver<T>, SourceStatistic<OperationObserver<? super T>> {
+
+  public Class<T> type();
+  
+  /**
+   * Return a {@ValueStatistic<Long>} returning the count for the given result.
+   * 
+   * @param result the result of interest 
+   * @return a {@code ValueStatistic} instance
+   */
+  public ValueStatistic<Long> statistic(T result);
+
+  public ValueStatistic<Long> statistic(Set<T> results);
+  
+  /**
+   * Return the count of operations with the given type.
+   * 
+   * @param type the result type
+   * @return the operation count
+   */
+  public long count(T type);
+
+  public long sum(Set<T> types);
+  
+  public long sum();
+}
