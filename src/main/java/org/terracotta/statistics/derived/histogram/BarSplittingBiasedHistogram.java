bb7c388f6f49b9f0f0952d34600ea2387561c31a
Chris Dennis <chris.w.dennis@gmail.com>
2018-03-22 11:52:29 -0400

    Update copyright statements

--- src/main/java/org/terracotta/statistics/derived/histogram/BarSplittingBiasedHistogram.java
+++ src/main/java/org/terracotta/statistics/derived/histogram/BarSplittingBiasedHistogram.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Terracotta, Inc..
+ * All content copyright Terracotta, Inc., unless otherwise indicated.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
